b1fbc85a1de237c0fe4dea66b8c6eb399081af8b
guillaume gallois <guillaume.gallois@gmail.com>
2016-04-15 12:27:20 +0200

    add asign examples

--- 12---objects/index.js
+++ 12---objects/index.js
@@ -5,3 +5,58 @@ var pizza = {
 };
 
 console.log(pizza);
+
+//----------------------------------
+const groupe = {
+  reference: {
+    codeAfc: 588,
+    numeroClient: 45471,
+    typeActivite: "D",
+    natureContrat: "GPD",
+    codeDevise: "EUR  "
+  },
+  referenceAcheteur: {
+    codeAcheteur: 1970645,
+    devise: "EUR",
+    afcach: 588
+  },
+  siren: 410140057,
+  limiteCredit: 2500000,
+  encoursGlobal: 92248.56,
+  encourga: 92248.56,
+  indsuspd: "N",
+  indstop: "N"
+
+}
+
+
+const acheteur = {
+  id: 13516,
+  name: "acheteur toto"
+}
+
+console.log(Object.assign({}, acheteur, { groupe }));
+
+
+
+/*
+{ id: 13516,
+name: 'acheteur toto',
+groupe: {
+    reference: {
+      codeAfc: 588,
+      numeroClient: 45471,
+      typeActivite: 'D',
+      natureContrat: 'GPD',
+      codeDevise: 'EUR  '
+    },
+    referenceAcheteur: { codeAcheteur: 1970645, devise: 'EUR', afcach: 588 },
+    siren: 410140057,
+    limiteCredit: 2500000,
+    encoursGlobal: 92248.56,
+    encourga: 92248.56,
+    indsuspd: 'N',
+    indstop: 'N'
+  }
+}
+*/
