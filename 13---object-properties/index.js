658d4bb841bb03aecc1ca2ccceddac81d73bc17d
Guillaume GALLOIS <guillaume.gallois@gmail.com>
2017-06-15 09:28:43 +0200

    Update index.js

--- 13---object-properties/index.js
+++ 13---object-properties/index.js
@@ -1,5 +1,9 @@
 var food = {
+  id: 'foodId',
   types: 'only pizza'
 };
 
 console.log(food.types);
+let food2 = {[food.id]: 'fruits'};
+console.log(food2); //{ foodId: 'fruits' }
+console.log(food2.foodId); //fruits
